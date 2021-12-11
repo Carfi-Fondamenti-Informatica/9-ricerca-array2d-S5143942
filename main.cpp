302be0fb9d60cf1293676382c583b622dfe8a691
S5143942 <92802547+S5143942@users.noreply.github.com>
2021-12-11 17:44:39 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,18 +3,17 @@
 using namespace std;
 
 int main() {
-int pos;
-  char a[10][20], f[1][20];
+char list [10][20];
+  char name[20];
+  int a=0;
   for (int i=0; i<10 ;i++){
-    cin >> a[i];
+    cin >>list[i];
   }
-  cin >> f[0];
-  
-  
-  if (ricerca(a, f, pos)){
-      cout << pos << endl;
+  cin >>name;
+  if (res(list, name,a)){
+      cout <<a;
   }else{
-    cout << "non presente" << endl;
+    cout << "non presente";
   }
 
   return 0;
