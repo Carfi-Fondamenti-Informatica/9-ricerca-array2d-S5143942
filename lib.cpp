5c4a136d181ec0e421064d20da206f1d8f547506
S5143942 <92802547+S5143942@users.noreply.github.com>
2021-12-11 15:00:43 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -4,11 +4,11 @@
 
 bool ricerca(char lista [10][20], char nome[1][20], int &pos){
   
-  for (int i=0; i<10 ; i++){
-      if (strcmp (lista [i], nome [0]) == 0){
+    for (int i=0; i<10 ; i++){
+       if (strcmp (lista [i], nome [0]) == 0){
           pos = i;
           return true;
-      }
-  }
-  return false
-  }
+       }
+   }
+   return false
+ }
