ad1fb90c07966062eb5c0ae05e3ed54ca170865c
S5143942 <92802547+S5143942@users.noreply.github.com>
2021-12-11 14:53:33 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,12 +5,17 @@ using namespace std;
 int main(){
 int pos;
   char a[10][20], f[1][20];
-  for (int i=0;; i<10 ;i++){
-    cin >>a[i];
+  for (int i=0; i<10 ;i++){
+    cin >> a[i];
   }
-  numeroMassimo (numeri, dim);
+cin >> f[0];
   
-  cout << numeroMassimo (numeri, dim ) << endl;
   
+  if(ricerca(a, f, pos,)){
+    cout << pos << endl;
+  }else{
+    cout << "non presente" << endl;
+  }
+
   return 0;
 }
