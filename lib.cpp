bfb52b51533cb22dc63dfe0c856401b4e1abc81c
S5143942 <92802547+S5143942@users.noreply.github.com>
2021-12-11 17:49:37 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,13 +1,14 @@
+
+
 #include "lib.h"
 #include <string.h>
-
-
 bool res (char list [10][20], char name[20], int &i){
   for (int i=0; i<10 ; i++){
-       if ((strcmp (list[i], name )) == 0){
-        
-          return true;
+       if((strcmp(list[i],name)) == 0){
+           return true;
        }
+    
+    
    }
-   return false
+   return false;
  }
