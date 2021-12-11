1625bfd8d290c0df214089e62397a690b4a6c76b
S5143942 <92802547+S5143942@users.noreply.github.com>
2021-12-11 14:30:23 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+int pos;
+  char a[10][20], f[1][20];
+  for (int i=0;; i<10 ;i++){
+    cin >>a[i];
+  }
+  numeroMassimo (numeri, dim);
+  
+  cout << numeroMassimo (numeri, dim ) << endl;
+  
   return 0;
 }
